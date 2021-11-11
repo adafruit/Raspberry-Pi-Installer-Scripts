ba7406516e0bdc3914f031e044ed43c6ef361a50
Margu <44941663+Margu86@users.noreply.github.com>
2021-11-11 15:09:54 +0100

    added RASPBERRY_PI_ZERO_2_W model

--- i2smic.py
+++ i2smic.py
@@ -14,9 +14,9 @@ I2S microphone support.
         shell.bail("Non-Raspberry Pi board detected.")
     pi_model = shell.get_board_model()
     print("{} detected.\n".format(pi_model))
-    if pi_model in ("RASPBERRY_PI_ZERO", "RASPBERRY_PI_ZERO_W", "RASPBERRY_PI_ZERO_2_W"):
+    if pi_model in ("RASPBERRY_PI_ZERO", "RASPBERRY_PI_ZERO_W"):
         pimodel_select = 0
-    elif pi_model in ("RASPBERRY_PI_2B", "RASPBERRY_PI_3B", "RASPBERRY_PI_3B_PLUS", "RASPBERRY_PI_3A_PLUS"):
+    elif pi_model in ("RASPBERRY_PI_2B", "RASPBERRY_PI_3B", "RASPBERRY_PI_3B_PLUS", "RASPBERRY_PI_3A_PLUS", "RASPBERRY_PI_ZERO_2_W"):
         pimodel_select = 1
     elif pi_model in ("RASPBERRY_PI_4B", "RASPBERRY_PI_CM4"):
         pimodel_select = 2
