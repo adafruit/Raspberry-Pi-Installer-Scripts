108f4e381b0ce29dfd92b0ce5486429b97f6e832
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-03-23 18:24:12 -0400

    docs(libgpiod): fix typo

--- libgpiod.py
+++ libgpiod.py
@@ -1,9 +1,7 @@
 # Instructions!
 # cd ~
 # sudo apt-get install python3-pip
-# sudo pip3 install --upgrade adafruit-python3-shell click 
-#   => NOTE: if the adafruit library fails, try installing
-#            the adafruit-python-shell package instead.
+# sudo pip3 install --upgrade adafruit-python-shell click
 # wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/libgpiod.py
 # sudo python3 libgpiod.py
 
